e11cbb0f0f97daec0642de47d817971827e1c2d0
안제욱 <112852022+lukas0409@users.noreply.github.com>
2024-04-13 18:02:47 +0900

    ✨ 20240120_stepper_planeheight.ino 업데이트

--- 20240120_stepper_planeheight.ino
+++ 20240120_stepper_planeheight.ino
@@ -5,37 +5,40 @@
 우측 버튼 : 하강
  */
 
+
 #include <Stepper.h> // 스텝모터 라이브러리
 
 // 변수 정의
 int up;
 int down;
 
-const int stepsPerRevolution = 1024; // 회전 각도 180도 
+const int stepsPerRevolution = 64; // 회전 각도 180도 
 Stepper myStepper(stepsPerRevolution,11,9,10,8); // 스텝모터 핀세팅
  
 void setup() {
-  pinMode(4,INPUT);
-  pinMode(5,INPUT);
+  pinMode(8,OUTPUT);
+  pinMode(9,OUTPUT);
+  pinMode(10,OUTPUT);
+  pinMode(11,OUTPUT);
 
-  myStepper.setSpeed(30); // 스텝모터 회전속도 30
+  myStepper.setSpeed(300); // 스텝모터 회전속도 30
+
+  Serial.begin(115200);
 }
 
 void loop() {
-  up = digitalRead(4);
-  down = digitalRead(5);
-
-  // 좌측 버튼 누를시 시계방향 회전
-  if (up == HIGH) {
-    for (int a=0; a <= 2; a++) {
-      myStepper.step(stepsPerRevolution); 
+  if (Serial.available()) {
+    char in = Serial.parseInt();
+    // 좌측 버튼 누를시 시계방향 회전
+    if(in == 1) {
+  for(int i=0; i<32; i++) {  // 64 * 32 = 2048 한바퀴
+    myStepper.step(stepsPerRevolution);
+  }
+    }
+    if(in == 2) {
+  for(int i=0; i<32; i++) {  // 64 * 32 = 2048 한바퀴
+    myStepper.step(-stepsPerRevolution);
     }
   }
-
-  // 우측 버튼 누를시 반시계방향 회전
-  else if (down == HIGH) {
-    for (int b=0; b <= 2; b++) {
-      myStepper.step(-stepsPerRevolution); 
-    }
-  }
+}
 }
